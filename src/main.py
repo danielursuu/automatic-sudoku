409a783cf31a36c53ab4b64380b6e1f67bc8f62d
danielursuu <danielursuu@gmail.com>
2019-12-05 16:26:39 +0200

    Added the possibility to read and show an image

---
+++ src/main.py
@@ -0,0 +1,22 @@
+import cv2
+
+def SUDOKU_IMAGE_PATH():
+    return "sudoku_images/test_1.jpg"
+
+def import_image_by_path(image_path):
+    img = cv2.imread(image_path)
+
+    return img
+
+def show_image(title, image):
+    cv2.imshow(title, image)
+    cv2.waitKey()
+
+
+
+def main():
+    original_image = import_image_by_path(SUDOKU_IMAGE_PATH())
+    show_image("Original", original_image)
+
+if __name__ == '__main__':
+    main()
