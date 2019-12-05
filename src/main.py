77045acf049ae80f9276bd968ef9327194fe56f3
danielursuu <danielursuu@gmail.com>
2019-12-05 17:53:41 +0200

    Get tresholded image

--- src/main.py
+++ src/main.py
@@ -8,15 +8,24 @@ def import_image_by_path(image_path):
 
     return img
 
+def get_thresholded_image(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray.copy(), (5,5), 0)
+    thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
+
+    return thresh
+
 def show_image(title, image):
     cv2.imshow(title, image)
     cv2.waitKey()
 
 
-
 def main():
     original_image = import_image_by_path(SUDOKU_IMAGE_PATH())
     show_image("Original", original_image)
+
+    processed_image = get_thresholded_image(original_image)
+    show_image("Treshold", processed_image)
 
 if __name__ == '__main__':
     main()
