25e97315c43678f77a1975f066187cfa27b34ec8
danielursuu <danielursuu@gmail.com>
2019-12-10 10:50:07 +0200

    Get max contour of the tresholded image

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def SUDOKU_IMAGE_PATH():
     return "resources/sudoku_images/test_1.jpg"
@@ -15,6 +16,30 @@ def get_thresholded_image(img):
 
     return thresh
 
+def get_mask_with_max_area(contours):
+    max_area = 0
+    mask = None
+
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if (area > max_area):
+            max_area = area
+            mask = contour
+
+    return mask
+
+def crop_grid(img):
+    contours, _ = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    mask = get_mask_with_max_area(contours)
+
+    bounding_rect = cv2.boundingRect(mask)
+    x, y, w, h = bounding_rect
+    oppening = np.zeros((h, w), np.float)
+    oppening[0:h, 0:w] = img[y:y + h, x:x + w]
+
+    return oppening, bounding_rect
+
+
 def show_image(title, image):
     cv2.imshow(title, image)
     cv2.waitKey()
@@ -26,6 +51,9 @@ def main():
 
     processed_image = get_thresholded_image(original_image)
     show_image("Treshold", processed_image)
+
+    crop, rectangle = crop_grid(processed_image)
+    show_image("Cropped", crop)
 
 if __name__ == '__main__':
     main()
